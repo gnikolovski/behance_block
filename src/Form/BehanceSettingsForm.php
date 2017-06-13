915119f91b58c94f43f38f3f8d6a7a7f7af2539b
Goran Nikolovski <nikolovski84@gmail.com>
2017-06-13 21:48:33 +0200

    Refactor config form

--- src/Form/BehanceSettingsForm.php
+++ src/Form/BehanceSettingsForm.php
@@ -13,7 +13,7 @@ use GuzzleHttp\Exception\ClientException;
  */
 
 /**
- * Defines a form that configures behance block settings.
+ * Defines a form that configures Behance Block settings.
  */
 class BehanceSettingsForm extends ConfigFormBase {
 
@@ -28,73 +28,68 @@ class BehanceSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return array('behance_block.settings');
+    return ['behance_block.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-
     $config = $this->config('behance_block.settings');
 
     // API key field.
-    $form['api_key'] = array(
+    $form['api_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('API Key'),
       '#required' => TRUE,
       '#description' => $this->t('Enter your API key. If you don\'t have one, visit <a target="_blank" href="https://www.behance.net/dev/register">this page</a> and get your Behance API key.'),
       '#default_value' => $config->get('api_key'),
-    );
+    ];
 
     // User ID or username field.
-    $form['user_id'] = array(
+    $form['user_id'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Username or User ID'),
       '#required' => TRUE,
-      '#description' => $this->t("Enter portfolio owner's ID or username."),
+      '#description' => $this->t("Enter the username or user ID of the project's owner."),
       '#default_value' => $config->get('user_id'),
-    );
+    ];
 
     // New tab checkbox.
-    $form['new_tab'] = array(
+    $form['new_tab'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Open links in new tab'),
-      '#description' => $this->t('Check if you want Behance links to be opened in the new tab.'),
+      '#description' => $this->t('Check this if you want Behance links to be opened in a new tab.'),
       '#default_value' => $config->get('new_tab'),
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
-
   }
 
   /**
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-
     $api_key = $form_state->getValue('api_key');
     $user_id = $form_state->getValue('user_id');
 
-    $isDataValid = $this->isDataValid($api_key, $user_id);
+    $is_data_valid = $this->isDataValid($api_key, $user_id);
 
-    if ($isDataValid == 403) {
-      $form_state->setErrorByName('api_key', $this->t('Your API key is not valid'));
+    if ($is_data_valid == 403) {
+      $form_state->setErrorByName('api_key', $this->t('Your API key is not valid.'));
     }
-
-    if ($isDataValid == 404) {
-      $form_state->setErrorByName('user_id', $this->t('User not found'));
+    elseif ($is_data_valid == 404) {
+      $form_state->setErrorByName('user_id', $this->t('User not found.'));
     }
-
+    elseif ($is_data_valid === NULL) {
+      $form_state->setErrorByName('api_key', $this->t('Unknown error.'));
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
-    $this->clearCacheFiles();
-
     $values = $form_state->getValues();
 
     $config = $this->config('behance_block.settings');
@@ -104,21 +99,18 @@ class BehanceSettingsForm extends ConfigFormBase {
     $config->save();
 
     parent::submitForm($form, $form_state);
-
   }
 
   /**
    * Check if API key and User ID are valid.
    */
   private function isDataValid($api_key, $user_id) {
-
     $client = new Client();
 
     try {
       $response = $client->get('https://api.behance.net/v2/users/' . $user_id . '?client_id=' . $api_key);
       $response_code = $response->getStatusCode();
-    }
-    catch (ClientException $e) {
+    } catch (ClientException $e) {
       $response = $e->getResponse();
       $response_code = $response->getStatusCode();
       watchdog_exception('behance_block', $e);
@@ -136,22 +128,10 @@ class BehanceSettingsForm extends ConfigFormBase {
     elseif ($response_code == 404) {
       return 404;
     }
-
-  }
-
-  /**
-   * Delete cache files.
-   */
-  private function clearCacheFiles() {
-
-    if (file_exists('public://behance_fields.json')) {
-      unlink('public://behance_fields.json');
+    // Unknown error.
+    else {
+      return NULL;
     }
-
-    if (file_exists('public://behance_projects.json')) {
-      unlink('public://behance_projects.json');
-    }
-
   }
 
 }
